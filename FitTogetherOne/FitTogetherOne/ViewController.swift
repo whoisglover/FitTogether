14ea1ce31f4f6bb5d44279c26057a298f34bc609
Josh <jrocnc@mail.missouri.edu>
2014-11-13 15:19:45 -0600

    another test commit

--- FitTogetherOne/FitTogetherOne/ViewController.swift
+++ FitTogetherOne/FitTogetherOne/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         // test comment
+        // another test
     }
 
     override func didReceiveMemoryWarning() {
