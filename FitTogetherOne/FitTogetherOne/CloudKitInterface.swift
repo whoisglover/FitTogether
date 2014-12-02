8b20ec6c51422229cc4dfe48821c2737017274e4
Danny Glover <djglover00@gmail.com>
2014-12-02 13:38:05 -0600

    started cloudkit custom class, described methods needed

---
+++ FitTogetherOne/FitTogetherOne/CloudKitInterface.swift
@@ -0,0 +1,37 @@
+//
+//  CloudKitInterface.swift
+//  FitTogetherOne
+//
+//  Created by Danny Glover on 12/2/14.
+//  Copyright (c) 2014 Glover LLC. All rights reserved.
+//
+
+import UIKit
+
+class CloudKitInterface: NSObject {
+    //methods we need: 
+    
+    //establish connection to container
+    //establish connection to public database
+    
+    //check if username found in nsUserDefaults - appdidfinishloadingwithoptions (I think)
+    //If not get a username - check to make sure unique in user records in public database
+    
+    //create user record with username
+    
+    //update user record with steps today !! need to grab steps from healthkit
+
+    //update user record with new daily goal
+    
+    //update user record with team reference when team is joined
+    
+    //create a team record when team created
+    
+    //create a challenge record when another team is challenged
+    
+    //update a challenge record when a team accepts a challenge
+    
+    //when steps are updated in cloudkit, check if the user has gained a badge
+    //if they have gained a badge update locally and to cloudkit
+    
+}
