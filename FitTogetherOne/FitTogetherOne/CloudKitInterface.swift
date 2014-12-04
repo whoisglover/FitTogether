d327f6e98ddefff3f0a84b7066eb20bfb4db66d3
Danny Glover <djglover00@gmail.com>
2014-12-04 12:58:57 -0600

    adding pseduocode for cloudkit flow

--- FitTogetherOne/FitTogetherOne/CloudKitInterface.swift
+++ FitTogetherOne/FitTogetherOne/CloudKitInterface.swift
@@ -9,29 +9,68 @@
 import UIKit
 
 class CloudKitInterface: NSObject {
-    //methods we need: 
+// MARK: Methods we need:
     
     //establish connection to container
     //establish connection to public database
     
+    
+    
     //check if username found in nsUserDefaults - appdidfinishloadingwithoptions (I think)
     //If not get a username - check to make sure unique in user records in public database
+    //when app is loaded the step update function should run and update the users steps from healthkit to cloudkit
     
     //create user record with username
     
     //update user record with steps today !! need to grab steps from healthkit
 
-    //update user record with new daily goal
+    //update user record with new daily goal !! local only no cloudkit needed
     
     //update user record with team reference when team is joined
     
     //create a team record when team created
     
-    //create a challenge record when another team is challenged
+    //create a challenge record when you (admin) challenge a team
     
     //update a challenge record when a team accepts a challenge
+    
     
     //when steps are updated in cloudkit, check if the user has gained a badge
     //if they have gained a badge update locally and to cloudkit
     
+// MARK: Cycle for updating team steps today and challenge
+    //1. run healthkit helper methods to update current users steps today and total steps (this needs to happen outside of cloudkit class i.e. wherever this is all kicked off)
+    //2. is this user on a team? if so: (again this is not dependent on cloudkit, can happen outside cloudkit class)
+    // if on a team, kick off the cloudkit methods below
+    //3. grab the team object, get the user id's array
+    //4. grab the all the user objects using the user id's array
+    //5. grab all the 'steps_today' from each user object, total all steps today and set for team steps today, update/save in icloud
+    //5.1 save all teammates and their steps to local data model to display on Team page
+    
+    //6. check if this user is involved in a challenge:
+    //7. use team id to query all challenges check if team id is equal to team a or team b in any current challenge
+    //8. if not, do nothing, wrap up skip to step 13
+    //9. if you are in a challenge:
+    //10. divide current user's team total steps by team user id's array.count
+    //11. update your team's average in challenge
+    //12. we need a single helper method to run at the end of the day, compare team a average vs team b and increment a score
+    //13. save all records to public database
+    
+ 
+// MARK: Connect to public data base
+    
+// MARK: Query the team record associated with current user
+    //grab current user from data model, use whatever is stored in team_id to query the team record
+    //return team record
+    
+// MARK: Query all users records from the user id's array (input: team record)
+    //create a team object for data model
+    //save each teammate into team data model
+
+// MARK: Query all challenges to see if any contain current user's team id
+    //return record for challenge or nil
+    
+// MARK: End of day calculations
+    
+// MARK: Save all records to update cloudkit database
 }
